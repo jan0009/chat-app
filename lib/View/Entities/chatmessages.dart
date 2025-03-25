37ba1960edf41fac25a39ed0fc7dca1bf39523bc
Jan Drescher <jadrit00@hs-esslingen.de>
2025-03-25 12:04:30 +0100

    feat: ChatMessages Entities

---
+++ lib/View/Entities/chatmessages.dart
@@ -0,0 +1,32 @@
+class ChatMessages {
+  final int id;
+  final String userid;
+  final String time;
+  final String chatid;
+  final String text;
+  final String usernick;
+  final String userhash;
+
+  ChatMessages({
+    required this.id,
+    required this.userid,
+    required this.time,
+    required this.chatid,
+    required this.text,
+    required this.usernick,
+    required this.userhash,
+  });
+
+  factory ChatMessages.fromJson(Map<String, dynamic> json) {
+
+    return ChatMessages(
+      id: json['id'],
+      userid: json['userid'],
+      time: json['time'],
+      chatid: json['chatid'],
+      text:  json['text'],
+      usernick: json['usernick'],
+      userhash: json['userhash'],
+    );
+  }
+}
